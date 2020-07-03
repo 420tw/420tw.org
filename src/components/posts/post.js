654845844a9ab67c76ec2e1e616ca7f7d36e9d33
Gary Lai <garylai1990@gmail.com>
2020-07-03 16:06:58 +0800

    :lipstick: (post) add padding to post

--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -7,7 +7,7 @@ const Post = ({ post }) => {
   const { title, content, publishedAt } = post
   const intl = useIntl()
   return (
-    <div className="max-w-3xl mx-auto my-8">
+    <div className="max-w-3xl mx-auto my-8 py-2 px-4">
       <h1 className="text-4xl">{title}</h1>
       <div className="text-sm dark:text-gray-300">
         {intl.formatMessage({ id: "published_at" })}{" "}
