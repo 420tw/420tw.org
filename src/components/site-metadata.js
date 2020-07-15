49beb1ecd9887e5d566980d5c5b66df30579a90d
Gary Lai <garylai1990@gmail.com>
2020-07-15 22:04:14 +0800

    fix(og): update og

--- src/components/site-metadata.js
+++ src/components/site-metadata.js
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet"
 import React from "react"
 import { useSiteMetadata } from "../hooks/use-site-metadata"
 import useLanguage from "../hooks/use-language"
-import ogImage from "../images/420Asia.org.png"
+import ogImage from "../images/open_graph.jpg"
 
 const SiteMetadata = ({ pathname }) => {
   const { title, description, siteUrl, twitter } = useSiteMetadata()
@@ -26,7 +26,7 @@ const SiteMetadata = ({ pathname }) => {
       <meta property="og:locale" content={currentLang} />
       <meta property="og:site_name" content={title} />
       <meta property="og:image" content={`${siteUrl}${ogImage}`} />
-      <meta property="og:image:alt" content="420Asia.org Logo" />
+      <meta property="og:image:alt" content="420tw.org Logo" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
 
